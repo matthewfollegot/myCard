c9b6927b78244aac71db3b21d078db29c834ac2a
Nicolas Abou Sawan <nicolasabousawan@gmail.com>
2018-03-04 11:07:09 -0500

    I am from the North, sometimes

--- TEXTRECEIVE.js
+++ TEXTRECEIVE.js
@@ -1,5 +1,14 @@
 var app = angular.module('mainApp',[]);
 
 app.controller('Values', function($scope,$http){
-  $http.get("10.21.233.121:5000/newContact/apple");
-})
+  $http.get("10.21.233.121:5000/createProfile/apple")
+  .then(function(resp){
+    console.log('SUCCESS: ', resp);
+  })
+  .catch(function(err){
+    console.log('FAILURE: ', err);
+  })
+  .finally(function(){
+    console.log('DEFAULT BEHAVIOUR');
+  });
+});
