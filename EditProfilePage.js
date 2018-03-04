ad938df492b4db141121574152a9628e91432c3a
Nicolas Abou Sawan <nicolasabousawan@gmail.com>
2018-03-03 22:10:38 -0500

    This is batch 2 of new files

---
+++ EditProfilePage.js
@@ -0,0 +1,11 @@
+var FName = "Insert JSON parameter";
+document.getElementById("FirstName").value = FName;
+
+var LName = "Insert JSON parameter";
+document.getElementById("LastName").value = LName;
+
+var Email = "Insert JSON parameter";
+document.getElementById("email").value = Email;
+
+var number = "Insert JSON parameter";
+document.getElementById("phoneNumber").value = number;
